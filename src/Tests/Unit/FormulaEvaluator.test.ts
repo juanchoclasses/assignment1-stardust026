fd99ccb0bbafb7a3ab209089d0354094063b98d5
Steven <zhangchenchen@ashitas-MacBook-Air.local>
2023-09-28 16:18:00 -0700

    Fix bad test case

--- src/Tests/Unit/FormulaEvaluator.test.ts
+++ src/Tests/Unit/FormulaEvaluator.test.ts
@@ -112,7 +112,7 @@ describe("FormulaEvaluator", () => {
           let result = recalc.result;
           let error = recalc.error;
 
-          expect(result).toEqual(3);
+          expect(result).toEqual(-1);
           expect(error).toEqual("");
         });
       });
@@ -128,7 +128,7 @@ describe("FormulaEvaluator", () => {
           let result = recalc.result;
           let error = recalc.error;
 
-          expect(result).toEqual(3);
+          expect(result).toEqual(2);
           expect(error).toEqual("");
         });
       });
@@ -174,7 +174,7 @@ describe("FormulaEvaluator", () => {
         let error = recalc.error;
 
         expect(result).toEqual(0);
-        expect(error).toEqual(ErrorMessages.missingParentheses);
+        expect(error).toEqual(ErrorMessages.invalidFormula);
       });
     });
 
